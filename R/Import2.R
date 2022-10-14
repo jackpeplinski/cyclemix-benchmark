5fba1a5c1d2dfa89b3fd7a019413623f7d9ef708
JackPeplinski <jackEpeplinski@gmail.com>
2022-10-13 23:20:11 -0400

    Add factor to sce

--- R/Import2.R
+++ R/Import2.R
@@ -5,10 +5,11 @@ require("CycleMix")
 counts <- read.table("./jackData/E-MTAB-2805.processed.1/G1_singlecells_counts.txt", header=TRUE)
 rownames(counts) <- counts$EnsemblGeneID 
 counts <- head(counts, -97) 
-test <- DataFrame(row.names = counts$EnsemblGeneID, feature_symbol = counts$AssociatedGeneName)
+# counts <- subset(counts, select = -c(EnsemblGeneID, EnsemblTranscriptID, AssociatedGeneName, GeneLength))
+# test <- DataFrame(row.names = counts$EnsemblGeneID, feature_symbol = counts$AssociatedGeneName)
 sce <- SingleCellExperiment(assays=list(counts=counts),
                             rowData = DataFrame(row.names = counts$EnsemblGeneID,
-                            feature_symbol = counts$AssociatedGeneName)) # is having not relevant colnames ok?
+                            feature_symbol = factor(counts$AssociatedGeneName))) # is having not relevant colnames ok?
 
 # Couldn't find the data set from the source 
 
