8cab6ace1a12e76dc29cc8dbbc7e15ebc960e4ea
JackPeplinski <jackEpeplinski@gmail.com>
2022-10-19 22:16:20 -0400

    Feat: Get code to run

--- R/Import2.R
+++ R/Import2.R
@@ -2,18 +2,45 @@ require("SingleCellExperiment")
 require("scater")
 require("CycleMix")
 
-counts <- read.table("./jackData/E-MTAB-2805.processed.1/G1_singlecells_counts.txt", header=TRUE)
-rownames(counts) <- counts$EnsemblGeneID 
-counts <- head(counts, -97) 
-# counts <- subset(counts, select = -c(EnsemblGeneID, EnsemblTranscriptID, AssociatedGeneName, GeneLength))
-# test <- DataFrame(row.names = counts$EnsemblGeneID, feature_symbol = counts$AssociatedGeneName)
-sce <- SingleCellExperiment(assays=list(counts=counts),
-                            rowData = DataFrame(row.names = counts$EnsemblGeneID,
-                            feature_symbol = factor(counts$AssociatedGeneName))) # is having not relevant colnames ok?
+test <- read.table(
+    "./jackData/E-MTAB-2805.processed.1/G1_singlecells_counts.txt",
+    header = TRUE
+)
 
-# Couldn't find the data set from the source 
+counts <- read.table(
+    "./jackData/E-MTAB-2805.processed.1/G1_singlecells_counts.txt",
+    header = TRUE
+)
 
-# ignore
-# ensemblGeneID <- counts$EnsemblGeneID
-# genes <- counts$AssociatedGeneName
-# counts <- subset(counts, select = -c(`EnsemblGeneID`,`EnsemblTranscriptID`, `AssociatedGeneName`, `GeneLength`) )
+rownames(counts) <- counts$EnsemblGeneID
+
+counts <- head(counts, -97)
+rowData <- DataFrame(
+    row.names = counts$EnsemblGeneID,
+    feature_symbol = factor(counts$AssociatedGeneName)
+)
+
+colData <- DataFrame(
+    row.names = colnames(counts)[5:100],
+    Species = factor("Mus musculus"),
+    cell_type1 = factor(
+        substr(colnames(counts)[5:100], 1, 2)
+    ), Source = factor("ESC")
+)
+counts <- subset(counts,
+    select = -c(
+        EnsemblGeneID,
+        EnsemblTranscriptID,
+        AssociatedGeneName,
+        GeneLength
+    )
+)
+sce <- SingleCellExperiment(
+    assays = list(counts = counts),
+    colData = colData, # look at split function
+    rowData = rowData
+)
+counts <- assay(sce, "counts")
+libsizes <- colSums(counts)
+size.factors <- libsizes / mean(libsizes)
+logcounts(sce) <- log2(t(t(counts) / size.factors) + 1)
