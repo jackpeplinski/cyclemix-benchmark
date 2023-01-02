5e179e383359de74f808daec53942855e160dbed
JackPeplinski <jackEpeplinski@gmail.com>
2023-01-02 12:57:14 -0700

    feat: remove all phase cells

--- jackR/benchmark.R
+++ jackR/benchmark.R
@@ -9,11 +9,10 @@ require("stringr")
 # Don't need to log normalize, each file is a cell.
 # Round fpkm for counts, and put fpkm into the logcounts
 gse <- function() {
-    # copy pasted this from xml
+    # copy/pasted this from gseParse
     col_data_xml <- data.frame(
-        Gsm = c("All phase", "All phase", "All phase", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "All phase", "All phase", "All phase", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "All phase", "All phase", "All phase", "All phase", "All phase", "All phase", "All phase", "All phase", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "S", "S", "S", "S", "S", "S", "S", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1"), # nolint
-        Species = c("Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus", "Mus musculus"), # nolint
-        cell_type1 = c("GSM1036480", "GSM1036481", "GSM1036482", "GSM1036483", "GSM1036484", "GSM1036485", "GSM1036486", "GSM1036487", "GSM1036488", "GSM1036489", "GSM1036490", "GSM1036491", "GSM1036492", "GSM1036493", "GSM1036494", "GSM1036495", "GSM1036496", "GSM1036497", "GSM1036498", "GSM1036499", "GSM1036500", "GSM1036501", "GSM1036502", "GSM1036503", "GSM1036504", "GSM1036505", "GSM1036506", "GSM1036507", "GSM1036508", "GSM1036509", "GSM1036510", "GSM1036511", "GSM1036512", "GSM1036513", "GSM1036514", "GSM1036515", "GSM1036516", "GSM1036517", "GSM1036518", "GSM1036519", "GSM1036520", "GSM1036521", "GSM1036522", "GSM1036523", "GSM1036524", "GSM1036525", "GSM1036526", "GSM1036527", "GSM1036528", "GSM1036529", "GSM1036530", "GSM1036531", "GSM1036532", "GSM1036533", "GSM1036534", "GSM1036535", "GSM1036536", "GSM1036537", "GSM1036538", "GSM1036539", "GSM1036540", "GSM1036541", "GSM1036542", "GSM1036543", "GSM1036544", "GSM1036545", "GSM1036546", "GSM1036547", "GSM1036548", "GSM1036549", "GSM1036550", "GSM1036551", "GSM1036552", "GSM1036553", "GSM1036554", "GSM1036555", "GSM1036556") # nolint
+        cell_type1 = c("G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "S", "S", "S", "S", "S", "S", "S", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G2/M", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1", "G1"), # nolint
+        gsm = c("GSM1036483", "GSM1036484", "GSM1036485", "GSM1036486", "GSM1036487", "GSM1036488", "GSM1036489", "GSM1036490", "GSM1036491", "GSM1036492", "GSM1036493", "GSM1036494", "GSM1036495", "GSM1036496", "GSM1036500", "GSM1036501", "GSM1036502", "GSM1036503", "GSM1036504", "GSM1036505", "GSM1036506", "GSM1036507", "GSM1036508", "GSM1036509", "GSM1036510", "GSM1036511", "GSM1036512", "GSM1036513", "GSM1036522", "GSM1036523", "GSM1036524", "GSM1036525", "GSM1036526", "GSM1036527", "GSM1036528", "GSM1036529", "GSM1036530", "GSM1036531", "GSM1036532", "GSM1036533", "GSM1036534", "GSM1036535", "GSM1036536", "GSM1036537", "GSM1036538", "GSM1036539", "GSM1036540", "GSM1036541", "GSM1036542", "GSM1036543", "GSM1036544", "GSM1036545", "GSM1036546", "GSM1036547", "GSM1036548", "GSM1036549", "GSM1036550", "GSM1036551", "GSM1036552", "GSM1036553", "GSM1036554", "GSM1036555", "GSM1036556") # nolint
     )
 
     files <- list.files(
@@ -41,13 +40,18 @@ gse <- function() {
                 gene.symbol
             )
         )
-
+        # print(col_data_xml["gsm"])
+        # print(col_data_xml$gsm)
+        print(colnames(col_data_xml))
+        # print(col_data_xml["cell_type1"])
         col_data <- DataFrame(
-            row.names =  
+            row.names = col_data_xml$gsm,
             Species = factor("Mus musculus"),
-            cell_type1 = col_data_xml,
+            cell_type1 = col_data_xml$cell_type1, # why the "1" at the end?
             Source = factor("ESC")
         )
+        # print(col_data)
+        # print(col_data["GSM1036531", ]) # verify that the correct phase is matched
 
         sce <- SingleCellExperiment(
             assays = list(counts = counts),
@@ -62,7 +66,7 @@ gse <- function() {
     }
 }
 
-# gse()
+gse()
 
 emtab_2805 <- function(file_name) {
     counts <- read.table(
@@ -113,10 +117,10 @@ emtab_2805_res <- cbind(
     emtab_2805("S_singlecells_counts")
 )
 
-output2 <- classifyCells(emtab_2805_res, MGeneSets$Cyclone)
-summary(factor(output2$phase))
-table(factor(output2$phase), emtab_2805_res$cell_type1)
-plotMixture(output2$fit[["G2M"]], BIC = TRUE)
+# output2 <- classifyCells(emtab_2805_res, MGeneSets$Cyclone)
+# summary(factor(output2$phase))
+# table(factor(output2$phase), emtab_2805_res$cell_type1)
+# plotMixture(output2$fit[["G2M"]], BIC = TRUE)
 
 marioni <- function() {
     counts <- read.table(
